5f4d006dd5a9da814f7398a906efde678bac07a5
Lijo <lijo.varghese@seqato.com>
2018-05-09 11:08:35 +0530

    created board game

--- iris/README.md
+++
@@ -1,2 +0,0 @@
-# machineLearn
-It contains machine learning projects
--- iris/src/base.py
+++ iris/src/base.py
@@ -1,13 +1,33 @@
 import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
 
 
 def read_csv():
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
     names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
     dataset = pandas.read_csv(url, names=names)
-    print(dataset.head(20))
-    print(dataset.shape)
-    print(dataset.describe())
+    # print(dataset.head(20))
+    # print(dataset.shape)
+    # print(dataset.describe())
+    # print(dataset.groupby('class').size())
+    # dataset.plot(kind='box', subplots=True, layout=(2, 2), sharex=False, sharey=False)
+    # plt.show()
+    # dataset.hist()
+    # plt.show()
+    # scatter_matrix(dataset)
+    # plt.show()
+    print(dataset.max())
 
 
 read_csv()
